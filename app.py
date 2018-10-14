404ebdbc4fb6cba13c5dc713724abc34363c812e
Ubuntu <ubuntu@ip-172-31-24-41.us-east-2.compute.internal>
2018-10-14 03:31:25 +0000

    websocket

--- app.py
+++ app.py
@@ -1,50 +1,83 @@
-import logging
-import tornado
-from flask import Flask, render_template
-from tornado.web import FallbackHandler
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from tornado.websocket import WebSocketHandler
+import redis
+from gevent import monkey
+monkey.patch_all()
 
-from data import Data
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1234567'
+db = redis.StrictRedis('localhost', 6379, 0)
+socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-def send_message(message):
-    for handler in ChatSocketHandler.socket_handlers:
-        try:
-            data = Data()
-            result = data.get_buy_price()
-            handler.write_message(message + '\' price is ' + str(result))
-        except:
-            logging.error('Error sending message', exc_info=True)
+@app.route('/localhost')
+def main():
+	return render_template('main.html')
 
 
-class ChatSocketHandler(WebSocketHandler):
-    socket_handlers = set()
-    def open(self):
-        ChatSocketHandler.socket_handlers.add(self)
-        send_message('A new user has entered the Coinbase lobby.')
+@socketio.on('connect')
+def ws_conn():
+	c = db.incr('user_count')
+	print('connecting', c)
+	socketio.emit('msg', {'count': c})
 
-    def on_close(self):
-        ChatSocketHandler.socket_handlers.remove(self)
-        send_message('A user has left the Coinbase lobby.')
 
-    def on_message(self, message):
-        send_message(message)
+@socketio.on('disconnect')
+def ws_disconn():
+        c = db.decr('user_count')
+	print('disconnecting', c)
+        socketio.emit('msg', {'count': c})
 
 
 if __name__ == '__main__':
-    applications = tornado.web.Application([# order matters! otherwise, everytying will go to flask server.
-                                            (r'/websocket', ChatSocketHandler),
-                                            (r'/', FallbackHandler, dict(fallback=WSGIContainer(app))),
-                                            ])
-    http_server = HTTPServer(applications)
-    http_server.listen(5000)
-    IOLoop.instance().start()
+	socketio.run(app, debug=True)
+ 
+# import logging
+# import tornado
+# from flask import Flask, render_template
+# from tornado.web import FallbackHandler
+# from tornado.wsgi import WSGIContainer
+# from tornado.httpserver import HTTPServer
+# from tornado.ioloop import IOLoop
+# from tornado.websocket import WebSocketHandler
+# 
+# from data import Data
+# 
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = '1234567'
+# 
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+# 
+# def send_message(message):
+#     for handler in ChatSocketHandler.socket_handlers:
+#         try:
+#             data = Data()
+#             result = data.get_buy_price()
+#             handler.write_message(message + '\' price is ' + str(result))
+#         except:
+#             logging.error('Error sending message', exc_info=True)
+# 
+# 
+# class ChatSocketHandler(WebSocketHandler):
+#     socket_handlers = set()
+#     def open(self):
+#         ChatSocketHandler.socket_handlers.add(self)
+#         send_message('A new user has entered the Coinbase lobby.')
+# 
+#     def on_close(self):
+#         ChatSocketHandler.socket_handlers.remove(self)
+#         send_message('A user has left the Coinbase lobby.')
+# 
+#     def on_message(self, message):
+#         send_message(message)
+# 
+# 
+# if __name__ == '__main__':
+#     applications = tornado.web.Application([# order matters! otherwise, everytying will go to flask server.
+#                                             (r'/websocket', ChatSocketHandler),
+#                                             (r'/', FallbackHandler, dict(fallback=WSGIContainer(app))),
+#                                             ])
+#     http_server = HTTPServer(applications)
+#     http_server.listen(5000)
+#     IOLoop.instance().start()
